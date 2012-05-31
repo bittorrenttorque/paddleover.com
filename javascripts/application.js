dcf8d43d19f8b2062643ae29109635e3124fb2fd
Patrick Williams <patrick@bittorrent.com>
2012-05-31 13:55:56 -0700

    replacing the hardcoded pwmckenna name with the provided name

--- javascripts/application.js
+++ javascripts/application.js
@@ -452,13 +452,13 @@
 
 		//twitter
 		(function() {
-			var link = 'http://paddleover.com?name=pwmckenna&cu=' + jQuery.jStorage.get('username') + '&cp=' + jQuery.jStorage.get('password');
+			var link = 'http://paddleover.com?name=' + jQuery.jStorage.get('name') + '&cu=' + jQuery.jStorage.get('username') + '&cp=' + jQuery.jStorage.get('password');
 			var text = 'Drag files from my computer to yours, and visa versa using #PaddleOver.'
 			$('.twitter_bubble').attr('href', 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(link) + '&text=' + text);
 		}());
 
 		$('.fb_bubble').click(function() {
-			var link = 'http://paddleover.com?name=pwmckenna&cu=' + jQuery.jStorage.get('username') + '&cp=' + jQuery.jStorage.get('password');
+			var link = 'http://paddleover.com?name=' + jQuery.jStorage.get('name') + '&cu=' + jQuery.jStorage.get('username') + '&cp=' + jQuery.jStorage.get('password');
 			var text = 'Drag files from my computer to yours, and visa versa.'
 			var description = 'Let friends add files to your computer, and do the same for them. Works both ways too, so you can take what you want!';
 			FB.init({appId: '353964634659536', xfbml: true, cookie: true});
