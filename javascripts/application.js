962b00ce4e209478d56c8defbdd8790c5722ad61
Patrick Williams <patrick@bittorrent.com>
2012-07-03 21:32:45 -0700

    fixing a null pointer bug

--- javascripts/application.js
+++ javascripts/application.js
@@ -308,13 +308,13 @@
 				greedy: 'true',
 				accept: _.bind(function(draggable) {
 					// Only allow drops of torrents
-					var torrent = draggable.data('torrent').id;
+					var torrent = draggable.data('torrent');
 					if(!draggable.data('bubble') || !torrent) {
 						return;
 					}
 
 					var addable = this.model.btapp.has('add');
-					var duplicate = this.model.btapp.has('torrent') && this.model.btapp.get('torrent').get(torrent.toUpperCase());
+					var duplicate = this.model.btapp.has('torrent') && this.model.btapp.get('torrent').get(torrent.id.toUpperCase());
 					var ret = addable && !duplicate;
 					return ret;
 				}, this),
