73e12836357fd7d0a703fecd729c9a4aff59abfc
Patrick Williams <patrick@bittorrent.com>
2012-08-05 23:40:37 -0700

    switching to use the new style of queries

--- javascripts/application.js
+++ javascripts/application.js
@@ -112,22 +112,22 @@
 
 	function getQueries() {
 		return [
-			'btapp/torrent/all/*/remove/',
-			'btapp/torrent/all/*/open_containing/',
-			'btapp/torrent/all/*/properties/all/uri/', 
-			'btapp/torrent/all/*/properties/all/name/', 
-			'btapp/torrent/all/*/properties/all/eta/', 
-			'btapp/torrent/all/*/properties/all/size/', 
-			'btapp/torrent/all/*/properties/all/progress/', 
-			'btapp/torrent/all/*/properties/all/added_on/', 
-			'btapp/browseforfiles/',
-			'btapp/create/', 
-			'btapp/settings/', 
-			'btapp/add/',
-			'btapp/events/',
-			'btapp/connect_remote/',
-			'btapp/stash/',
-			'btapp/showview/'
+			['btapp','torrent','all','*','remove'],
+			['btapp','torrent','all','*','open_containing'],
+			['btapp','torrent','all','*','properties','all','uri'], 
+			['btapp','torrent','all','*','properties','all','name'], 
+			['btapp','torrent','all','*','properties','all','eta'], 
+			['btapp','torrent','all','*','properties','all','size'], 
+			['btapp','torrent','all','*','properties','all','progress'], 
+			['btapp','torrent','all','*','properties','all','added_on'], 
+			['btapp','browseforfiles'],
+			['btapp','create'], 
+			['btapp','settings'], 
+			['btapp','add'],
+			['btapp','events'],
+			['btapp','connect_remote'],
+			['btapp','stash'],
+			['btapp','showview']
 		];
 	}
 
